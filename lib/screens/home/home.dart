6519fc118f87ec4d0b1caaa8d36f2092daf1d281
michael99hernan <michael99hernandez@gmail.com>
2021-01-20 02:18:30 -0600

    added home buttons

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -8,21 +8,38 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: backgroundColor,
-        appBar: customAppBar(
-          "Hello",
-          IconButton(icon: Icon(Icons.arrow_back), onPressed: null),
-          <Widget>[
-            FlatButton.icon(
-                onPressed: () async {
-                  await _authService.signOut();
-                },
-                icon: Icon(Icons.person),
-                label: Text(
-                  'sign out',
-                  style: TextStyle(color: Colors.black),
-                ))
+      backgroundColor: backgroundColor,
+      appBar: customAppBar(
+        "Hello",
+        IconButton(icon: Icon(Icons.arrow_back), onPressed: null),
+        <Widget>[
+          FlatButton.icon(
+              onPressed: () async {
+                await _authService.signOut();
+              },
+              icon: Icon(Icons.person),
+              label: Text(
+                'sign out',
+                style: TextStyle(color: Colors.black),
+              ))
+        ],
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            RaisedButton(
+              color: Colors.blue,
+              onPressed: null,
+              child: Text('Create review'),
+            ),
+            RaisedButton(
+              color: Colors.blue,
+              onPressed: null,
+              child: Text('View review'),
+            ),
           ],
-        ));
+        ),
+      ),
+    );
   }
 }

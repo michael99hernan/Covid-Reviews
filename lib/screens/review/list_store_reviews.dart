076db798c7d7407512c4d6a195bf222b384f1353
michael99hernan <michael99hernandez@gmail.com>
2021-01-26 16:36:10 -0600

    started class

--- lib/screens/review/list_store_reviews.dart
+++ lib/screens/review/list_store_reviews.dart
@@ -1,1 +1,14 @@
 //This is where you will build a list of reviews
+import 'package:flutter/material.dart';
+
+class StoreReviews extends StatefulWidget {
+  @override
+  _StoreReviewsState createState() => _StoreReviewsState();
+}
+
+class _StoreReviewsState extends State<StoreReviews> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

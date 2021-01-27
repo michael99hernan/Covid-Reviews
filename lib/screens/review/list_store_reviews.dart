96c7956c63165950c7fbfad733e01f6902d65ca2
michael99hernan <michael99hernandez@gmail.com>
2021-01-26 18:18:43 -0600

    added loading icon

--- lib/screens/review/list_store_reviews.dart
+++ lib/screens/review/list_store_reviews.dart
@@ -3,6 +3,7 @@ import 'package:covid_reviews/models/review.dart';
 import 'package:covid_reviews/services/review_services.dart';
 import 'package:covid_reviews/shared/appbar.dart';
 import 'package:covid_reviews/shared/constants.dart';
+import 'package:covid_reviews/shared/loading.dart';
 import 'package:flutter/material.dart';
 
 class StoreReviews extends StatefulWidget {
@@ -18,15 +19,8 @@ class _StoreReviewsState extends State<StoreReviews> {
   // TODO: This is hard coded value for a storeId for testing. We will change this next sprint.
   String storeId = 'kK0yUEMx9hTLZa9xeSFQ';
 
-  // This function is called when the page is loaded
-  @override
-  void initState() {
-    super
-        .initState(); // This calls the original init state (no need to worry about this)
-    getReviews(); //This gets reviews from database
-  }
-
-  Future<void> getReviews() async {
+  /// Calls the review service and assigns it to reviewList
+  Future getReviews() async {
     reviewList = await _reviewService.listReviewsByStore(storeId);
   }
 
@@ -39,20 +33,36 @@ class _StoreReviewsState extends State<StoreReviews> {
         null,
         null,
       ),
-      body: ListView.builder(
-        itemCount: reviewList.length,
-        itemBuilder: (context, index) {
-          return Card(
-            child: ListTile(
-              // TODO: Once noel is done with code call his page when clicked.
-              // TODO: Add the date
-              // TODO: Add the ratings
-              onTap: () {},
-              title: Text(reviewList[index].authorName),
-              subtitle: Text(reviewList[index].text),
-            ),
-          );
-        },
+      body: Container(
+        child: FutureBuilder(
+          //The future builder allows us to call the get reviews function
+          future: getReviews(), // Calls the function to get the list of reviews
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              //While loading the reviews...
+              return Loading(); // display the loading symbol
+            } else {
+              //Once done loading display the list of reviews
+              //THIS IS WHERE MOST OF THE CODE GOES
+              return ListView.builder(
+                itemCount: reviewList.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: ListTile(
+                      // TODO: Once noel is done with code call his page when clicked.
+                      // TODO: Add the date
+                      // TODO: Add the ratings
+                      onTap: () {},
+                      title: Text(reviewList[index].authorName),
+                      subtitle: Text(reviewList[index].text),
+                    ),
+                  );
+                },
+              );
+              //MOST OF THE CODE WILL BE ABOVE HERE
+            }
+          },
+        ),
       ),
     );
   }
